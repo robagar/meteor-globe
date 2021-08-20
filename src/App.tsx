9d93221439a53b4021124acdd022d5e31f8a4f95
Rob Agar <robagar@fastmail.net>
2021-08-20 10:57:22 +0100

    spherical globe, single colour map texture

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Canvas } from '@react-three/fiber'
+import { useTexture } from "@react-three/drei"
 import './App.css';
 
 function App() {
+  const material = useTexture({
+    map: 'textures/2_no_clouds_4k.jpeg'
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas frameloop="demand">
+        <ambientLight intensity={0.1} />
+        <directionalLight color="white" position={[0, 0, 5]} />
+        <mesh>
+          <sphereGeometry args={[2, 32, 32]}/>
+          <meshStandardMaterial {...material} />
+        </mesh>
+      </Canvas>
     </div>
   );
 }
